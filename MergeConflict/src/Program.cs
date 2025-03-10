42df717ea8047885222df8fe0bb5a6a52e9e5a26
Jerzy <s28706@pjwstk.edu.pl>
2025-03-10 14:52:52 +0100

    Added function for average value

--- MergeConflict/src/Program.cs
+++ MergeConflict/src/Program.cs
@@ -3,5 +3,18 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Goodbye World");
+        double[] arr = [1.0, 2.0, 4.0];
+        Console.WriteLine("Avg value: " + avgValue(arr));
+    }
+
+    public static double avgValue(double[] arr)
+    {
+        double sum = 0.0;
+        foreach (double numb in arr)
+        {
+            sum += numb;
+        }
+        
+        return sum / arr.Length;
     }
 }
